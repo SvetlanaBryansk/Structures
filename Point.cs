9a4828d0803d7405254533eb276bc398c4fbdcc8
Светлана <123038012+SvetlanaBryansk@users.noreply.github.com>
2023-02-20 02:51:31 +0300

    Add files via upload

---
+++ Point.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+struct Point
+{
+    public int X;
+    public int Y;
+
+    public Point(int x, int y)
+    {
+        X = x;
+        Y = y;
+    }
+    public double GetDistance(Point point)
+    {
+        int dx = this.X - point.X;
+        int dy = this.Y - point.Y;
+        return Math.Sqrt(dx * dx + dy * dy);
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Structures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point a = new Point(1, 1);
+            Point b = new Point(2, 5);
+
+            Console.WriteLine(a.GetDistance(b));
+        }
+
+        private static double GetDistance(Point a, Point b)
+        {
+            int x = a.X -b.X;
+            int y = a.Y -b.Y;
+
+            return Math.Sqrt(x * x + y * y);
+        }
+    }
+}
